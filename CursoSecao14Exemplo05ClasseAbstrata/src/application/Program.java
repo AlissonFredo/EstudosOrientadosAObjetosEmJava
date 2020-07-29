470444462fa8bf0d03ace62b62464b44db1e03ba
Alisson Fredo <douglasalissonsf@gmail.com>
2020-07-29 14:54:19 -0300

    programa cursosecao14exemplo05classeabstrata v2 foi codificado

--- CursoSecao14Exemplo05ClasseAbstrata/src/application/Program.java
+++ CursoSecao14Exemplo05ClasseAbstrata/src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -8,10 +11,27 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1020, "Alex", 1000.0);
-		Account acc2 = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-
+		List<Account> list = new ArrayList<>();
+		
+		list.add(new SavingsAccount(1023, "Maria", 1000.0, 0.01));
+		list.add(new BusinessAccount(1003, "Bob", 1000.0, 500.0));
+		list.add(new SavingsAccount(1004, "Alex", 500.0, 0.01));
+		list.add(new BusinessAccount(1005, "Pedro", 500.0, 500.0));
+		
+		double sun = 0.0;
+		for(Account acc: list) {
+			sun += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sun);
+		
+		for(Account acc: list) {
+			acc.deposit(10.0);
+		}
+		
+		for(Account acc: list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 		
 	}
 
